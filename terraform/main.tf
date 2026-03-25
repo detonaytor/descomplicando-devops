e753aedce01fbadb7d5a34b907134a029c135574
detonaytor <detonaytor.si@gmail.com>
2026-03-24 23:47:35 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 resource "google_container_cluster" "my_autopilot_cluster" {
   name     = "linuxtips"
-  location = "us-central1" # or your desired region
+  location = "us-central1-f" # or your desired region
   enable_autopilot = true
 }
 
 resource "google_container_cluster" "my_autopilot_cluster_giropops" {
   name     = "giropops"
-  location = "us-central1" # or your desired region
+  location = "us-central1-f" # or your desired region
   enable_autopilot = true
 }
